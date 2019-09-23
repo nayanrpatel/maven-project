f11284399a3ef69deb966bbf0948a7e725dfdb82
Patel, Nayan (Non-Employee) <Nayan.Patel@firstdata.com>
2019-09-22 20:35:31 -0400

    Removed comment and added info

--- server/src/main/java/com/example/Greeter.java
+++ server/src/main/java/com/example/Greeter.java
@@ -12,6 +12,11 @@ public class Greeter {
 
   }
 
+    /**
+     *
+     * @param Greater Message
+     * @return string message
+     */
   public String greet(String someone) {
     return String.format("Hello, %s!", someone);
   }
